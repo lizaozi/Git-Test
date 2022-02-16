a06f7312498d1a9f96a42fe979558dbda8d9b496
Li <Li@qq.com>
2022-02-16 19:46:19 +0800

    first commit

---
+++ src/main/java/edu/zhku/GitTest.java
@@ -0,0 +1,11 @@
+package edu.zhku;
+
+/**
+ * @author lzw
+ * @date 2022/2/16 - 19:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
